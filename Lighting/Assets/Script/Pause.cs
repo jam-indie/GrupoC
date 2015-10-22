9bd1bcbb513026d242ef97235d997bae0d2ce031
HugoOlaitan <hugosinple@gmail.com>
2015-10-22 01:18:09 -0200

    Pause esta OK! Para ativar o pause pressione P. Para desativar o pause pressione O.
    Quando os botões estiverem prontos,substitua o pressionar da letra P e O por 
    colisões com os botões pausar e resumir. Assim o código vai funcionar da mesma 
    forma.
    
    Resolução alterada para: 720x480

---
+++ Lighting/Assets/Script/Pause.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Pause : MonoBehaviour 
+{
+	public KeyCode pause;
+	public KeyCode SairPause;
+
+	// Use this for initialization
+	void Start () 
+	{
+		//me.timeScale == 1;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey(pause))
+		{		
+			Time.timeScale = 0;
+		}
+
+		if(Input.GetKey(SairPause))
+		{		
+			Time.timeScale = 1;
+		}
+	
+	}
+}
